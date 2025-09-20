f664ce155a2f6693efa195e90badda89dcb7ca43
CarloBattista <carlitobatti@gmail.com>
2025-09-20 16:37:03 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,7 @@ export default {
   methods: {
     async getUser() {
       try {
+        console.log('test');
         const { data, error } = await supabase.auth.getUser();
 
         if (!error) {
