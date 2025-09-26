8a9f2f57770a0fb101b70204efc0d478747f9416
CarloBattista <carlitobatti@gmail.com>
2025-09-26 20:31:26 +0200

    feat(landing): add basic layout and branding to landing page

--- src/views/Landing/Landing-page.vue
+++ src/views/Landing/Landing-page.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>Landing page</div>
+  <div class="w-full h-svh flex flex-col gap-4 items-center justify-center">
+    <img src="/_resources/logo/badge_core.svg" alt="Keyp" draggable="false" class="h-[156px]" />
+    <h1 class="text-5xl font-bold">Landing Page</h1>
+    <p class="text-base font-normal">Work in progress</p>
+  </div>
 </template>
 
 <script>
